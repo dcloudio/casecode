6807fafcf126aa4ea5e664b3cfe2c7372b457e3d
git_robot <git_robot@dcloud.io>
2026-04-24 19:07:12 +0800

    Squashed 'hello-uni-app-x/' changes from 4d6427e89..33af27734
    33af27734 test: 更新provider自动化脚本
    
    git-subtree-dir: hello-uni-app-x
    git-subtree-split: 33af2773483144e0296abec23701839f04a2f3f6

--- pages/API/provider/provider.test.js
+++ pages/API/provider/provider.test.js
@@ -23,26 +23,22 @@ describe("provider", () => {
     await page.waitFor(1000);
     let providerIds = await page.data('data.providerIds')
 
-    expect(providerIds).toEqual(expect.arrayContaining(['wxpay', 'alipay', 'system']))
+    expect(providerIds).toEqual(expect.arrayContaining(['wxpay', 'alipay', 'system', 'weixin']))
     if (!isHarmony) {
       expect(providerIds).toEqual(expect.arrayContaining(['tencent']))
     } else {
       expect(providerIds).toEqual(expect.arrayContaining(['huawei']))
-      expect(providerIds).toEqual(expect.arrayContaining(['weixin']))
     }
-    expect(providerIds).toHaveLength(isHarmony ? 6 : 4)
+    expect(providerIds).toHaveLength(6)
 
     let providerObjects = await page.data('data.providerObjects')
 
-    expect(providerObjects).toEqual(expect.arrayContaining(['微信支付', '支付宝', '系统定位']))
+    expect(providerObjects).toEqual(expect.arrayContaining(['微信支付', '支付宝', '系统定位', '微信登录', '微信分享']))
     if (!isHarmony) {
       expect(providerObjects).toEqual(expect.arrayContaining(['腾讯定位']))
     } else {
       expect(providerObjects).toEqual(expect.arrayContaining(['华为登录']))
-      expect(providerObjects).toEqual(expect.arrayContaining(['微信登录']))
-      expect(providerObjects).toEqual(expect.arrayContaining(['微信分享']))
     }
-
-    expect(providerIds).toHaveLength(isHarmony ? 6 : 4)
+    expect(providerObjects).toHaveLength(6)
   });
 });
